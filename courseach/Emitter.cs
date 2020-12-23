03da9fe89920a2bb15113c98aa9d94b67ffbc835
Ashuuya <vladimirshizumi@yandex.ru>
2020-12-23 20:14:37 +0800

    1 commit

---
+++ courseach/Emitter.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing; // чтобы исплоьзовать Graphics
+
+namespace courseach
+{
+    class Emitter
+    {
+        List<Particle> particles = new List<Particle>();
+        public int MousePositionX;
+        public int MousePositionY;
+
+        public float GravitationX = 0;
+        public float GravitationY = 0; // пусть гравитация будет силой один пиксель за такт. ОТКЛЮЧЕНО
+
+        public List<IImpactPoint> impactPoints = new List<IImpactPoint>();
+
+        public void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                particle.Life -= 1; // уменьшаю здоровье
+
+                if (particle.Life < 0) // если здоровье кончилось
+                {
+                    // тоже не трогаем
+                    particle.Life = 20 + Particle.rand.Next(100);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+
+                    /* это убираем
+                    particle.Direction = Particle.rand.Next(360);
+                    particle.Speed = 1 + Particle.rand.Next(10);
+                    */
+
+                    /* ЭТО ДОБАВЛЯЮ, тут сброс состояния частицы */
+                    var direction = (double)Particle.rand.Next(360);
+                    var speed = 1 + Particle.rand.Next(10);
+
+                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+                    /* конец ЭТО ДОБАВЛЯЮ  */
+
+                    // это не трогаем
+                    particle.Radius = 2 + Particle.rand.Next(10);
+                }
+                else
+                {
+                    // каждая точка по-своему воздействует на вектор скорости
+                    foreach (var point in impactPoints)
+                    {
+                        point.ImpactParticle(particle);
+                    }
+
+                    // гравитация воздействует на вектор скорости, поэтому пересчитываем его
+                    particle.SpeedX += GravitationX;
+                    particle.SpeedY += GravitationY;
+
+                    // и добавляем новый, собственно он даже проще становится, 
+                    // так как теперь мы храним вектор скорости в явном виде и его не надо пересчитывать
+                    particle.X += particle.SpeedX;
+                    particle.Y += particle.SpeedY;
+                }
+            }
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 200)
+                {
+                    // а у тут уже наш новый класс используем
+                    var particle = new Particle.ParticleColorful();
+                    // ну и цвета меняем
+                    particle.FromColor = Color.Yellow;
+                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+
+            // добавил генерацию частиц
+            // генерирую не более 10 штук за тик
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 200) // пока частиц меньше 500 генерируем новые
+                {
+                    var particle = new Particle();
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break; // а если частиц уже 500 штук, то ничего не генерирую
+                }
+            }
+        }
+
+        public void Render(Graphics g)
+        {
+            // не трогаем
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+
+            foreach (var point in impactPoints) // тут теперь  impactPoints
+            {
+                /* это больше не надо
+                g.FillEllipse(
+                    new SolidBrush(Color.Red),
+                    point.X - 5,
+                    point.Y - 5,
+                    10,
+                    10
+                );
+                */
+                point.Render(g); // это добавили
+            }
+        }
+
+
+    }
+}
---
+++ courseach/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace courseach
+{
+    public partial class Form1 : Form
+    {
+        Emitter emitter = new Emitter(); // добавили эмиттер
+        public Form1()
+        {
+            InitializeComponent();
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            // гравитон
+            emitter.impactPoints.Add(new GravityPoint
+            {
+                X = (float)(picDisplay.Width * 0.25),
+                Y = picDisplay.Height / 2
+            });
+
+            // в центре антигравитон
+            emitter.impactPoints.Add(new AntiGravityPoint
+            {
+                X = picDisplay.Width / 2,
+                Y = picDisplay.Height / 2
+            });
+
+            // снова гравитон
+            emitter.impactPoints.Add(new GravityPoint
+            {
+                X = (float)(picDisplay.Width * 0.75),
+                Y = picDisplay.Height / 2
+            });
+        }
+
+        //// добавил функцию обновления состояния системы
+        //private void UpdateState()
+        //{
+
+        //}
+
+        //// функция рендеринга
+        //private void Render(Graphics g)
+        //{
+        //    // утащили сюда отрисовку частиц
+        //    foreach (var particle in particles)
+        //    {
+        //        particle.Draw(g);
+        //    }
+        //}
+
+        // ну и обработка тика таймера, тут просто декомпозицию выполнили
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            emitter.UpdateState(); // тут теперь обновляем эмиттер
+
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.Black);
+                emitter.Render(g); // рендерим систему
+            }
+
+            picDisplay.Invalidate();
+        }
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            // а тут в эмиттер передаем положение мыфки
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
+        }
+    }
+}
